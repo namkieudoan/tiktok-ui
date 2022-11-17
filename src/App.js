84cab027671bb05284831e67b6bc27dfcec55221
kieudoannam <kieunam2808@gmail.com>
2022-11-17 09:11:03 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+// thẻ fragment để chứa layout:null (upload page)
+import { Fragment } from 'react'
+
 function App() {
-  return (
-    <h1>React App</h1>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if(route.layout){
+                          Layout = route.layout;
+                        }else if(route.layout ===null){
+                          Layout = Fragment
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                      <Layout>
+                                          <Page />
+                                      </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
