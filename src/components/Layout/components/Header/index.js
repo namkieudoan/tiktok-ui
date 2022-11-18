32e19246a8dd24ece813ceef2ab4b0def914a7df
kieudoannam <kieunam2808@gmail.com>
2022-11-18 09:30:58 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,26 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx= classNames.bind(styles)
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            {/* logo, search... */}
+            <div className={cx('logo')}>
+                <img src = {images.logo} alt="tiktok"/>
+            </div>
+            <div className={cx('search')}>
+                <input type="text" placeholder='Search accounts and videos' spellCheck={false}/>
+                <span>x</span>
+                <button className={cx('search-btn')}>
+                    Search
+                </button>
+            </div>
+            <div className={cx('action')}>
+                <button>upload</button>
+                <button>login</button>
+            </div>
         </div>
     </header>;
 }
