3dbdcb04d836b694c258697764387666b8608544
kieudoannam <kieunam2808@gmail.com>
2022-11-23 15:16:42 +0700

    Xây dựng UI phần Header #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,11 +2,21 @@ import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faEllipsisVertical, faKeyboard, faLanguage, faMoon, faPlus, faQuestion, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faEllipsisVertical,
+    faKeyboard,
+    faLanguage,
+    faMoon,
+    faPlus,
+    faQuestion,
+    faSearch,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
+import images from '~/assets/images';
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
-import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
@@ -15,30 +25,54 @@ const cx = classNames.bind(styles);
 const MENU_LIST = [
     {
         icon: <FontAwesomeIcon icon={faLanguage} />,
-        title: "English",
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faQuestion} />,
-        title: "Feeback and help",
-        to: "/feeback"
+        title: 'Feeback and help',
+        to: '/feeback',
     },
     {
         icon: <FontAwesomeIcon icon={faKeyboard} />,
-        title: "Keyboard shortcuts"
+        title: 'Keyboard shortcuts',
     },
     {
         icon: <FontAwesomeIcon icon={faMoon} />,
-        title: "Dark theme"
+        title: 'Dark theme',
+    },
+];
+//handle logic
+const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+        case 'language':
+            //handle change language
+            break;
+        default:
     }
-]
+};
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
-    useEffect(()=> {
-        setTimeout(()=>{
+    useEffect(() => {
+        setTimeout(() => {
             setSearchResult([]);
-            return setSearchResult
-        },0);
-    },[])
+            return setSearchResult;
+        }, 0);
+    }, []);
 
     return (
         <header className={cx('wrapper')}>
@@ -48,15 +82,15 @@ function Header() {
                 </div>
                 <Tippy
                     interactive
-                    visible= {searchResult.length > 0}
+                    visible={searchResult.length > 0}
                     render={(attrs) => (
                         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                             <PopperWrapper>
                                 <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem/>
-                                <AccountItem/>
-                                <AccountItem/>
-                                <AccountItem/>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
                             </PopperWrapper>
                         </div>
                     )}
@@ -74,11 +108,13 @@ function Header() {
                     </div>
                 </Tippy>
                 <div className={cx('action')}>
-                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>Upload</Button>
-                    <Button primary >Log in</Button>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        Upload
+                    </Button>
+                    <Button primary>Log in</Button>
 
-                    <Menu items = {MENU_LIST}>
-                        <button className ={cx('more-btn')}>
+                    <Menu items={MENU_LIST} onChange={handleMenuChange}>
+                        <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
                     </Menu>
